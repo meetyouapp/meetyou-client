4b21c3b89b25369a55dc22b9119ff958f29e6290
tuwankuoza <twkoza@gmail.com>
2021-10-20 02:58:47 +0700

    integration profile

--- client/stores/reducers/index.js
+++ client/stores/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from "redux";
 import interestsReducer from "./interestsReducer";
 import usersReducer from "./usersReducer";
 import chatsReducer from "./chatsReducer";
+import profileReducer from './profileReducer'
+
 const reducer = combineReducers({
   interestsState: interestsReducer,
   usersState: usersReducer,
   chatsState: chatsReducer,
+  profileState: profileReducer,
 });
 
 export default reducer;
