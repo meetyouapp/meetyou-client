9b8c10a3d775557804f78905cd69f8c35058f8e5
theodorewaskito <theodorewaskito@gmail.com>
2021-10-20 21:11:08 +0700

    penambahan info di detail

--- client/apis/api.js
+++ client/apis/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 export const instance = axios.create({
   // baseURL: "http://192.168.1.190:3001", //rani
   baseURL: "http://192.168.1.5:3001",
@@ -8,15 +7,6 @@ export const instance = axios.create({
 });
 
 // const localIP = '192.168.0.176'
-=======
->>>>>>> 6e48d65a2a6924f11f61ced9a50ac05e219c047d
 // export const instance = axios.create({
-//   baseURL: "http://192.168.1.190:3001", //rani
-//   // baseURL: "http://192.168.1.9:3001",
-//   // baseURL: "http://localhost::3001",
+//   baseURL: `http://${localIP}:3001`,
 // });
-
-const localIP = '192.168.0.176'
-export const instance = axios.create({
-  baseURL: `http://${localIP}:3001`,
-});
